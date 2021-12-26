69bcb55cb2faa57d28dd9d01a677b7ffbe5519bf
zly8686 <zly86@qq.com>
2021-12-26 22:50:12 +0800

    Add files via upload

---
+++ cn.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+mkdir -p /tmp/smartdns/
+
+
+wget -O /tmp/smartdns/china.conf https://cdn.staticaly.com/gh/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget -O /tmp/smartdns/apple.conf https://cdn.staticaly.com/gh/felixonmars/dnsmasq-china-list/master/apple.china.conf
+wget -O /tmp/smartdns/google.conf https://cdn.staticaly.com/gh/felixonmars/dnsmasq-china-list/master/google.china.conf
+wget -O /tmp/smartdns/bogus-nxdomain.conf https://cdn.staticaly.com/gh/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf
+
+#合并
+cat /tmp/smartdns/apple.conf >> /tmp/smartdns/china.conf 2>/dev/null
+cat /tmp/smartdns/google.conf >> /tmp/smartdns/china.conf 2>/dev/null
+
+#删除不符合规则的域名
+sed -i "s/^server=\/\(.*\)\/[^\/]*$/nameserver \/\1\/china/g;/^nameserver/!d" /tmp/smartdns/china.conf 2>/dev/null
+
+mv -f /tmp/smartdns/china.conf  /etc/smartdns/smartdns-domains.china.conf
+mv -f /tmp/smartdns/bogus-nxdomain.conf  /etc/smartdns/nxdomain.conf
+rm -rf /tmp/smartdns/
+
+/etc/init.d/smartdns reload
